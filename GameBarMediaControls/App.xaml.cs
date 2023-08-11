ebf8f3c96a3953c130048cc7503adf1a66fa4056
LordElias <kerlinelias@gmail.com>
2023-08-11 14:26:16 +0200

    created repo

---
+++ GameBarMediaControls/App.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Gaming.XboxGameBar;
+
+namespace GameBarMediaControls
+{
+    /// <summary>
+    /// Stellt das anwendungsspezifische Verhalten bereit, um die Standardanwendungsklasse zu ergänzen.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        private XboxGameBarWidget widget1 = null;
+
+        /// <summary>
+        /// Initialisiert das Singletonanwendungsobjekt. Dies ist die erste Zeile von erstelltem Code
+        /// und daher das logische Äquivalent von main() bzw. WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        protected override void OnActivated(IActivatedEventArgs args) {
+            XboxGameBarWidgetActivatedEventArgs widgetArgs = null;
+            if (args.Kind == ActivationKind.Protocol) {
+                var protocolArgs = args as IProtocolActivatedEventArgs;
+                string scheme = protocolArgs.Uri.Scheme;
+                if (scheme.Equals("ms-gamebarwidget")) {
+                    widgetArgs = args as XboxGameBarWidgetActivatedEventArgs;
+                }
+            }
+            if (widgetArgs != null) {
+                //
+                // Activation Notes:
+                //
+                //    If IsLaunchActivation is true, this is Game Bar launching a new instance
+                // of our widget. This means we have a NEW CoreWindow with corresponding UI
+                // dispatcher, and we MUST create and hold onto a new XboxGameBarWidget.
+                //
+                // Otherwise this is a subsequent activation coming from Game Bar. We MUST
+                // continue to hold the XboxGameBarWidget created during initial activation
+                // and ignore this repeat activation, or just observe the URI command here and act 
+                // accordingly.  It is ok to perform a navigate on the root frame to switch 
+                // views/pages if needed.  Game Bar lets us control the URI for sending widget to
+                // widget commands or receiving a command from another non-widget process. 
+                //
+                // Important Cleanup Notes:
+                //    When our widget is closed--by Game Bar or us calling XboxGameBarWidget.Close()-,
+                // the CoreWindow will get a closed event.  We can register for Window.Closed
+                // event to know when our particular widget has shutdown, and cleanup accordingly.
+                //
+                // NOTE: If a widget's CoreWindow is the LAST CoreWindow being closed for the process
+                // then we won't get the Window.Closed event.  However, we will get the OnSuspending
+                // call and can use that for cleanup.
+                //
+                if (widgetArgs.IsLaunchActivation) {
+                    var rootFrame = new Frame();
+                    rootFrame.NavigationFailed += OnNavigationFailed;
+                    Window.Current.Content = rootFrame;
+
+                    // Create Game Bar widget object which bootstraps the connection with Game Bar
+                    widget1 = new XboxGameBarWidget(
+                        widgetArgs,
+                        Window.Current.CoreWindow,
+                        rootFrame);
+                    rootFrame.Navigate(typeof(MainPage));
+
+                    Window.Current.Closed += Widget1Window_Closed;
+
+                    Window.Current.Activate();
+                }
+                else {
+                    // You can perform whatever behavior you need based on the URI payload.
+                }
+            }
+        }
+
+        private void Widget1Window_Closed(object sender, Windows.UI.Core.CoreWindowEventArgs e) {
+            widget1 = null;
+            Window.Current.Closed -= Widget1Window_Closed;
+        }
+
+        /// <summary>
+        /// Wird aufgerufen, wenn die Anwendung durch den Endbenutzer normal gestartet wird. Weitere Einstiegspunkte
+        /// werden z. B. verwendet, wenn die Anwendung gestartet wird, um eine bestimmte Datei zu öffnen.
+        /// </summary>
+        /// <param name="e">Details über Startanforderung und -prozess.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // App-Initialisierung nicht wiederholen, wenn das Fenster bereits Inhalte enthält.
+            // Nur sicherstellen, dass das Fenster aktiv ist.
+            if (rootFrame == null)
+            {
+                // Frame erstellen, der als Navigationskontext fungiert und zum Parameter der ersten Seite navigieren
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Zustand von zuvor angehaltener Anwendung laden
+                }
+
+                // Den Frame im aktuellen Fenster platzieren
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // Wenn der Navigationsstapel nicht wiederhergestellt wird, zur ersten Seite navigieren
+                    // und die neue Seite konfigurieren, indem die erforderlichen Informationen als Navigationsparameter
+                    // übergeben werden
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Sicherstellen, dass das aktuelle Fenster aktiv ist
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
+        /// Wird aufgerufen, wenn die Navigation auf eine bestimmte Seite fehlschlägt
+        /// </summary>
+        /// <param name="sender">Der Rahmen, bei dem die Navigation fehlgeschlagen ist</param>
+        /// <param name="e">Details über den Navigationsfehler</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Wird aufgerufen, wenn die Ausführung der Anwendung angehalten wird.  Der Anwendungszustand wird gespeichert,
+        /// ohne zu wissen, ob die Anwendung beendet oder fortgesetzt wird und die Speicherinhalte dabei
+        /// unbeschädigt bleiben.
+        /// </summary>
+        /// <param name="sender">Die Quelle der Anhalteanforderung.</param>
+        /// <param name="e">Details zur Anhalteanforderung.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Anwendungszustand speichern und alle Hintergrundaktivitäten beenden
+            widget1 = null;
+            deferral.Complete();
+        }
+    }
+}
---
+++ GameBarMediaControls/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Control;
+using System.Threading;
+
+namespace GameBarMediaControls
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            init();
+        }
+
+        private GlobalSystemMediaTransportControlsSessionManager gsmtcsm = null;
+        private IDictionary<string, GlobalSystemMediaTransportControlsSession> sessions = null;
+        private GlobalSystemMediaTransportControlsSession session = null;
+        private string selectedItem = null;
+
+
+        private async void init() {
+            gsmtcsm = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+            gsmtcsm.SessionsChanged += Gsmtcsm_SessionsChanged;
+            sessionsCombo.SelectionChanged += SessionsCombo_SelectionChanged;
+
+            GetSessions();
+        }
+
+        private void Gsmtcsm_SessionsChanged(GlobalSystemMediaTransportControlsSessionManager sender, SessionsChangedEventArgs args) {
+            sessionsCombo.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, GetSessions);
+        }
+
+        private void GetSessions() {
+            sessions = gsmtcsm.GetSessions().ToDictionary(s => s.SourceAppUserModelId);
+
+            sessionsCombo.Items.Clear();
+            foreach (var key in sessions.Keys) {
+                sessionsCombo.Items.Add(key);
+            }
+
+            if(selectedItem != null && sessions.Keys.Contains(selectedItem)) sessionsCombo.SelectedItem = selectedItem;
+            else sessionsCombo.SelectedIndex = 0;
+
+            session = sessions[sessionsCombo.SelectedItem as string];
+        }
+
+        private void SessionsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (sessionsCombo.SelectedItem == null) return;
+            session = sessions[sessionsCombo.SelectedItem as string];
+            selectedItem = sessionsCombo.SelectedItem as string;
+        }
+
+
+        private async void Button_Click_PlayPause(object sender, RoutedEventArgs e) {
+            if(session != null) await session.TryTogglePlayPauseAsync();
+        }
+
+        private async void Button_Click_SkipNext(object sender, RoutedEventArgs e) {
+            if (session != null) await session.TrySkipNextAsync();
+        }
+
+        private async void Button_Click_SkipPrev(object sender, RoutedEventArgs e) {
+            if (session != null) await session.TrySkipPreviousAsync();
+        }
+
+        private async void Button_Click_Stop(object sender, RoutedEventArgs e) {
+            if (session != null) await session.TryStopAsync();
+        }
+    }
+}
